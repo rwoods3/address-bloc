3efe5e8b5044912dd4166e54427fe205e0405707
Raymond K. Woods <raymondkwoods@hotmail.com>
2018-11-10 12:06:41 -0800

    assignment 2: add remove_entry def and spec

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,8 @@ require_relative 'entry'
      end
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+  def remove_entry(name, phone_number, email)
+    entries.delete_if { |entry| entry.name === name && entry.phone_number === phone_number && entry.email === email }
+  end
  end
